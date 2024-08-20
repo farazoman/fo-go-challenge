afca8aa2457deb59a253a04dd710fad11451d6d3
Faraz Oman <farazoman@gmail.com>
2024-08-20 00:13:32 -0600

    Create terraform code to provision aws resources

--- main.tf
+++ main.tf
@@ -1,280 +1,241 @@
-
-# TODO implement, code is copied from another repo
-terraform { #this is we tell terraform we config AWS
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.63"
     }
   }
 }
 
-# TODO creds cleanup
-# TODO modularize files 
-# TODO is the public subnet safe... is it needed? -- yes because the private subnet routes outbound internet traffic requests to the public subnet
-# TODO rename names to have a or b sufixed
+variable "aws_region" {
+  default = "us-west-2"
+  type = string
+}
 
-provider "aws" { #give user secret access here
-  region     = "us-west-2"
+provider "aws" {
+  region     = var.aws_region
   access_key = ""
   secret_key = ""
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  # ID ???
-  tags = {
-    Name    = "vpc-mt"
-    Purpose = "multi-tenant"
-  }
-}
-
-# TODO simplify route creation with loop or module
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/18"
-  availability_zone = "us-west-2d"
-  # THe below causes issues if trying to setup nodes on public subnet
-  # map_public_ip_on_launch = true # does this do anything, should I delete, not sure its valid for my use-case
-  tags = {
-    Name                     = "subnet-mt-pub"
-    Purpose                  = "multi-tenant"
-    "kubernetes.io/role/elb" = 1
-    # kubernetes.io/role/elb	
-  }
-}
-
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.64.0/18"
-  availability_zone = "us-west-2d"
-  tags = {
-    Name                              = "subnet-mt-priv"
-    Purpose                           = "multi-tenant"
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
-}
-
-resource "aws_subnet" "publicA" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.128.0/18"
-  availability_zone = "us-west-2a"
-  # map_public_ip_on_launch = true # does this do anything, should I delete, not sure its valid for my use-case
-  tags = {
-    Name                     = "subnet-mt-pub"
-    Purpose                  = "multi-tenant"
-    "kubernetes.io/role/elb" = 1
-    # kubernetes.io/role/elb	
-  }
-}
-
-resource "aws_subnet" "privateA" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.192.0/18"
-  availability_zone = "us-west-2a"
-  tags = {
-    Name                              = "subnet-mt-priv"
-    Purpose                           = "multi-tenant"
-    "kubernetes.io/role/internal-elb" = 1
-  }
-}
-
-
-resource "aws_internet_gateway" "public" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name    = "igw-main"
-    Purpose = "multi-tenant"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name    = "Public Subnet Route Table"
-    Purpose = "multi-tenant"
-  }
-}
-
-
-resource "aws_route" "igw_route" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.public.id
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_nat_gateway" "public" {
-  subnet_id     = aws_subnet.public.id
-  allocation_id = aws_eip.public.id
-  tags = {
-    Name    = "Public NAT"
-    Purpose = "multi-tenant"
-  }
-}
-
-resource "aws_eip" "public" {
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name    = "Private Subnet Route Table"
-    Purpose = "multi-tenant"
-  }
-}
-
-
-resource "aws_route" "nat_route" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.public.id
-}
-
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
-
-## AZ a
-
-resource "aws_route_table" "publicA" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name    = "Public Subnet Route Table A"
-    Purpose = "multi-tenant"
-  }
-}
-
-
-resource "aws_route" "igw_routeA" {
-  route_table_id         = aws_route_table.publicA.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.public.id
-}
-
-resource "aws_route_table_association" "publicA" {
-  subnet_id      = aws_subnet.publicA.id
-  route_table_id = aws_route_table.publicA.id
-}
-
-resource "aws_nat_gateway" "publicA" {
-  subnet_id     = aws_subnet.publicA.id
-  allocation_id = aws_eip.publicA.id
-  tags = {
-    Name    = "Public NAT"
-    Purpose = "multi-tenant"
-  }
-}
-
-resource "aws_eip" "publicA" {
-}
-
-resource "aws_route_table" "privateA" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name    = "Private Subnet Route Table"
-    Purpose = "multi-tenant"
-  }
-}
-
-
-resource "aws_route" "nat_routeA" {
-  route_table_id         = aws_route_table.privateA.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.publicA.id
-}
-
-
-resource "aws_route_table_association" "privateA" {
-  subnet_id      = aws_subnet.privateA.id
-  route_table_id = aws_route_table.privateA.id
-}
-
-
-
-
-##### KUBERNETES #####
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
 
     principals {
       type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
+      identifiers = ["lambda.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_iam_role" "eks" {
-  name               = "eks-cluster-example"
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-
-  tags = {
-    Name    = "eks basic role"
-    Purpose = "multi-tenant"
-  }
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks.name
+variable "zip_path" {
+   default = "out/zip/lambda.zip"
+   type = string
 }
 
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks.name
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "out/bootstrap"
+  output_path = var.zip_path
 }
 
-resource "aws_eks_cluster" "eks" {
-  name     = "eks_multi"
-  role_arn = aws_iam_role.eks.arn
+resource "aws_lambda_function" "notify_lambda" {
+  filename      = "${path.module}/${var.zip_path}"
+  function_name = "transaction_summary_notifier"
+  role          = aws_iam_role.iam_for_lambda.arn 
+  handler       = "bootstrap"
+  
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  vpc_config {
-    subnet_ids = [aws_subnet.public.id, aws_subnet.private.id, aws_subnet.publicA.id, aws_subnet.privateA.id]
+  runtime = "provided.al2023"
+
+  environment {
+    variables = {
+      NOTIFY_BUCKET_NAME = aws_s3_bucket.notify_s3.id
+      TO_EMAIL           = "farazoman@gmail.com"
+    }
   }
-
-  tags = {
-    Purpose = "multi-tenant"
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.group,
   ]
 }
 
-# TODO find an easier way to install calls to nodegroup module without tofu init being needed
+resource "aws_s3_bucket" "notify_s3" {
+  bucket = "fo-notify-transactions"
+  force_destroy = true
+}
 
-module "base_node_group" {
-  source          = "./modules/node_group"
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group      = {
-    name = "base_ng"
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notify_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.notify_s3.arn
+}
+
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.notify_s3.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.notify_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "inbox/"
+    filter_suffix       = ".csv"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
+
+data "aws_iam_policy_document" "lambda_s3_read" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+    ]
+
+    resources = [ "${aws_s3_bucket.notify_s3.arn}/inbox/*", aws_s3_bucket.notify_s3.arn ]
   }
 }
 
-# module "second_node_group" {
-#   source          = "./modules/node_group"
-#   cluster_name    = aws_eks_cluster.eks.name
-#   node_group      = {
-#     name = "second_ng"
-#   }
-# }
+resource "aws_iam_policy" "lambda_s3_read" {
+  name        = "lambda_s3_read"
+  path        = "/"
+  description = "IAM policy for reading inbox from notify s3 bucket"
+  policy      = data.aws_iam_policy_document.lambda_s3_read.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_read" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_s3_read.arn
+}
+
+
+// Logging Config
+variable "lambda_function_name" {
+  default = "lambda_function_name"
+}
+
+resource "aws_cloudwatch_log_group" "group" {
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 1
+}
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+// Access to Secrets
+data "aws_iam_policy_document" "lambda_secrets" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+
+    resources = [ data.aws_secretsmanager_secret.sendgrid_secret.arn ]
+  }
+}
+
+data "aws_secretsmanager_secret" "sendgrid_secret" {
+  name = "SENDGRID_KEY"
+}
+
+resource "aws_iam_policy" "lambda_secrets" {
+  name        = "lambda_secrets"
+  path        = "/"
+  description = "IAM policy for getting secret value from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_secrets.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "lambda_secrets" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_secrets.arn
+}
+
+// Dynamo DBs
+resource "aws_dynamodb_table" "transaction_table" {
+  name           = "Transactions"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "GlobalTransactionId"
+
+  attribute {
+    name = "GlobalTransactionId"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "user_table" {
+  name           = "Users"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+}
+
+data "aws_iam_policy_document" "lambda_dynamo" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+    ]
+
+    resources = [
+      aws_dynamodb_table.transaction_table.arn,
+      aws_dynamodb_table.user_table.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_dynamo" {
+  name        = "lambda_dynamo"
+  path        = "/"
+  description = "IAM policy for lambda to access dynamo"
+  policy      = data.aws_iam_policy_document.lambda_dynamo.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamo" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_dynamo.arn
+}
